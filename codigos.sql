e48c15c93912765a0f875651806c38cc0194ba7e
Eduardo Ferreira <eduardosilvaferreira4@gmail.com>
2025-05-15 20:28:23 -0300

    Update codigos.sql

--- codigos.sql
+++ codigos.sql
@@ -1,1 +1,3 @@
-
+/*Questão 01. 
+Crie um procedimento denominado salaryHistogram, que distribua as frequências dos salários dos Professores em intervalos (Histograma).
+O número de intervalos será calculado de acordo com o parâmetro de entrada do procedimento. Exemplo: EXEC dbo.salaryHistogram 5;*/
